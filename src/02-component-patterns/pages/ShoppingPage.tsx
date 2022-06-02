c4a4af0c24b14e692f8888b099e93ce2dfae5237
Dave <david.magania@gmail>
2022-06-02 15:34:21 -0500

    Component Compound listo

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,36 @@
+import { ProductButtons, ProductImage, ProductTitle } from "../components"
+ import ProductCard from '../components';
+
+const product = {
+    id:'1',
+    title: 'Coffee Mug - 01',
+    img: './coffee-mug.png'
+}
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+        <h1>Shopping Store</h1>
+        <hr />
+
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap'
+            }}>
+                
+            <ProductCard product={ product }>
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons   />
+            </ProductCard>
+
+            <ProductCard product={ product }>
+                <ProductCard.Image />
+                <ProductCard.Title />
+                <ProductCard.Buttons />
+            </ProductCard>
+        </div>
+    </div>
+  )
+}
